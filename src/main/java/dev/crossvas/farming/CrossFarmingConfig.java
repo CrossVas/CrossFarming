72babb71b21ad3c7a2ea7a928acd5ef280f797d2
CrossVas <cross.wanted.1.4@gmail.com>
2024-01-22 20:34:19 +0900

    config organization

--- src/main/java/dev/crossvas/farming/CrossFarmingConfig.java
+++ src/main/java/dev/crossvas/farming/CrossFarmingConfig.java
@@ -55,51 +55,53 @@ public class CrossFarmingConfig {
         HANDLER = CarbonConfig.CONFIGS.createConfig(CONFIG);
         HANDLER.register();
 
-        ConfigSection CROP = CONFIG.add("crop");
+        ConfigSection FARMS = CONFIG.add("farms");
+
+        ConfigSection CROP = FARMS.addSubSection("crop");
         CROP.setComment("Crop Farm Config");
         CROP_FARM_SECONDS_TICK = CROP.addInt("crop_farm_seconds_tick", 5).setMin(0);
         CROP_FARM_ENERGY_USAGE = CROP.addInt("crop_farm_energy_usage", 20).setMin(0);
         CROP_COMBINE_SECONDS_TICK = CROP.addInt("crop_combine_seconds_tick", 10).setMin(0);
         CROP_COMBINE_ENERGY_USAGE = CROP.addInt("crop_combine_energy_usage", 30).setMin(0);
 
-        ConfigSection TREE = CONFIG.add("tree");
+        ConfigSection TREE = FARMS.addSubSection("tree");
         TREE.setComment("Tree Farm Config");
         TREE_FARM_SECONDS_TICK = TREE.addInt("tree_farm_seconds_tick", 5).setMin(0);
         TREE_FARM_ENERGY_USAGE = TREE.addInt("tree_farm_energy_usage", 20).setMin(0);
         TREE_COMBINE_SECONDS_TICK = TREE.addInt("tree_combine_seconds_tick", 2).setMin(0);
         TREE_COMBINE_ENERGY_USAGE = TREE.addInt("tree_combine_energy_usage", 30).setMin(0);
 
-        ConfigSection INFERNAL = CONFIG.add("infernal");
+        ConfigSection INFERNAL = FARMS.addSubSection("infernal");
         INFERNAL.setComment("Infernal Farm Config");
         INFERNAL_FARM_SECONDS_TICK = INFERNAL.addInt("infernal_farm_seconds_tick", 5).setMin(0);
         INFERNAL_FARM_ENERGY_USAGE = INFERNAL.addInt("infernal_farm_energy_usage", 20).setMin(0);
         INFERNAL_COMBINE_SECONDS_TICK = INFERNAL.addInt("infernal_combine_seconds_tick", 10).setMin(0);
         INFERNAL_COMBINE_ENERGY_USAGE = INFERNAL.addInt("infernal_combine_energy_usage", 30).setMin(0);
 
-        ConfigSection PEAT = CONFIG.add("peat");
+        ConfigSection PEAT = FARMS.addSubSection("peat");
         PEAT.setComment("Resin Farm Config");
         PEAT_FARM_SECONDS_TICK = PEAT.addInt("peat_farm_seconds_tick", 5).setMin(0);
         PEAT_FARM_ENERGY_USAGE = PEAT.addInt("peat_farm_energy_usage", 20).setMin(0);
         PEAT_COMBINE_SECONDS_TICK = PEAT.addInt("peat_combine_seconds_tick", 10).setMin(0);
         PEAT_COMBINE_ENERGY_USAGE = PEAT.addInt("peat_combine_energy_usage", 30).setMin(0);
 
-        ConfigSection RESIN = CONFIG.add("resin");
+        ConfigSection RESIN = FARMS.addSubSection("resin");
         RESIN.setComment("Resin Farm Config");
         RESIN_FARM_SECONDS_TICK = RESIN.addInt("resin_farm_seconds_tick", 5).setMin(0);
         RESIN_FARM_ENERGY_USAGE = RESIN.addInt("resin_farm_energy_usage", 20).setMin(0);
         RESIN_COMBINE_ENERGY_USAGE = RESIN.addInt("resin_combine_energy_usage", 30).setMin(0);
 
-        ConfigSection GOURD = CONFIG.add("gourd");
+        ConfigSection GOURD = FARMS.addSubSection("gourd");
         GOURD.setComment("Gourd Farm Config");
         GOURD_COMBINE_ENERGY_USAGE = GOURD.addInt("gourd_combine_seconds_tick", 20).setMin(0);
         GOURD_COMBINE_ENERGY_USAGE = GOURD.addInt("gourd_combine_energy_usage", 30).setMin(0);
 
-        ConfigSection SUCCULENT = CONFIG.add("succulent");
+        ConfigSection SUCCULENT = FARMS.addSubSection("succulent");
         SUCCULENT.setComment("Succulent Farm Config");
         GOURD_COMBINE_ENERGY_USAGE = SUCCULENT.addInt("succulent_combine_seconds_tick", 30).setMin(0);
         GOURD_COMBINE_ENERGY_USAGE = SUCCULENT.addInt("succulent_combine_energy_usage", 30).setMin(0);
 
-        ConfigSection COCOA = CONFIG.add("cocoa");
+        ConfigSection COCOA = FARMS.addSubSection("cocoa");
         COCOA.setComment("Cocoa Farm Config");
         COCOA_COMBINE_ENERGY_USAGE = COCOA.addInt("cocoa_combine_seconds_tick", 10).setMin(0);
         COCOA_COMBINE_ENERGY_USAGE = COCOA.addInt("cocoa_combine_energy_usage", 30).setMin(0);
